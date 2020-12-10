a308ca107af9191889c4dc55b7a013b581ff73a9
wangshan <shan.wang@aminer.cn>
2020-12-10 17:31:03 +0800

    增加import学者接口

--- blade-service/mk-adata/src/main/java/org/springblade/adata/service/impl/ExpertServiceImpl.java
+++ blade-service/mk-adata/src/main/java/org/springblade/adata/service/impl/ExpertServiceImpl.java
@@ -16,6 +16,7 @@
  */
 package org.springblade.adata.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -146,6 +147,121 @@ public class ExpertServiceImpl extends BaseServiceImpl<ExpertMapper, Expert> imp
 		body.put("schema", schema);
 		requestBody.add(body);
 		String res = MagicRequest.getInstance().magic(requestBody.toString());
+		return R.data(res);
+	}
+
+	@Override
+	public R<String> importDetail(String id) {
+		JSONArray requestBody = new JSONArray();
+		JSONObject body = new JSONObject();
+
+		JSONObject parameters = new JSONObject();
+		JSONArray ids = new JSONArray();
+		ids.add(id);
+		parameters.put("ids", ids);
+
+
+		JSONObject schema = new JSONObject();
+		JSONArray person = new JSONArray();
+		person.add("id");
+		person.add("name");
+		person.add("name_zh");
+		person.add("avatar");
+		person.add("links");
+
+		JSONObject profile_obj = new JSONObject();
+		JSONArray profile = new JSONArray();
+		profile.add("titles");
+		profile.add("phone");
+		profile.add("fax");
+		profile.add("email");
+		profile.add("affiliation");
+		profile.add("affiliation_zh");
+		profile.add("address");
+		profile.add("homepage");
+		profile.add("gender");
+		profile.add("lang");
+		profile.add("edu");
+		profile.add("work");
+		profile.add("bio");
+		profile.add("bio_zh");
+		profile.add("position");
+		profile.add("position_zh");
+		profile_obj.put("profile", profile);
+		person.add(profile_obj);
+		schema.put("person", person);
+
+		body.put("action", "personapi.get");
+		body.put("parameters", parameters);
+		body.put("schema", schema);
+		requestBody.add(body);
+		String res = MagicRequest.getInstance().magic(requestBody.toString());
+		JSONObject resObj = JSON.parseObject(res);
+		Expert expert = new Expert();
+		JSONArray dataArray = resObj.getJSONArray("data");
+		JSONObject tempObj = dataArray.getJSONObject(0);
+		JSONArray data = tempObj.getJSONArray("data");
+		JSONObject d = data.getJSONObject(0);
+		String name = d.getString("name");
+		String nameZh = d.getString("name_zh");
+		String titles = d.getString("titles");
+		JSONObject p = d.getJSONObject("profile");
+		String phone = p.getString("phone");
+		String fax = p.getString("fax");
+		String email = p.getString("email");
+		String affiliation = p.getString("affiliation");
+		String affiliation_zh = p.getString("affiliation_zh");
+		String address = p.getString("address");
+		String homepage = p.getString("homepage");
+		JSONObject links = d.getJSONObject("links");
+		JSONObject google = links.getJSONObject("gs");
+		JSONObject resource = links.getJSONObject("resource");
+		JSONArray resource_link = resource.getJSONArray("resource_link");
+		JSONObject first = resource_link.getJSONObject(0);
+		JSONObject second = resource_link.getJSONObject(1);
+		String hp = "";
+		String dblp = "";
+		if (first.getString("id").equals("hp")) {
+			hp = first.getString("url");
+		} else {
+			hp = second.getString("url");
+		}
+		if (first.getString("id").equals("dblp")) {
+			dblp = first.getString("url");
+		} else {
+			dblp = second.getString("url");
+		}
+		String gs = google.getString("url");
+		String gender = p.getString("gender");
+		String language = p.getString("language");
+		String avatar = d.getString("avatar");
+		String edu = p.getString("edu");
+		String work = p.getString("work");
+		String bio = p.getString("bio");
+		String bioZh = p.getString("bio_zh");
+
+		expert.setName(name);
+		expert.setNameZh(nameZh);
+		expert.setTitles(titles);
+		expert.setPhone(phone);
+		expert.setFax(fax);
+		expert.setEmail(email);
+		expert.setAffiliation(affiliation);
+		expert.setAffiliationZh(affiliation_zh);
+		expert.setAddress(address);
+		expert.setHomepage(homepage);
+		expert.setHp(hp);
+		expert.setGs(gs);
+		expert.setDblp(dblp);
+		expert.setGender(gender);
+		expert.setLanguage(language);
+		expert.setAvatar(avatar);
+		expert.setEdu(edu);
+		expert.setWork(work);
+		expert.setBio(bio);
+		expert.setBioZh(bioZh);
+
+		saveOrUpdate(expert);
 		return R.data(res);
 	}
 }
