b123c1d57d78d49e4b1fb8eaca41acd60039800e
yangkailun <2362080354@qq.com>
2020-12-10 17:54:17 +0800

    删除没有用到的类

--- blade-service-api/mk-adata-api/src/main/java/org/springblade/adata/feign/IExpertClient.java
+++ blade-service-api/mk-adata-api/src/main/java/org/springblade/adata/feign/IExpertClient.java
@@ -18,14 +18,11 @@ package org.springblade.adata.feign;
 
 import org.springblade.adata.entity.Expert;
 import org.springblade.common.constant.LauncherConstant;
-import org.springblade.core.mp.support.BladePage;
-import org.springblade.adata.entity.Notice;
 import org.springblade.core.tool.api.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Notice Feign接口类
