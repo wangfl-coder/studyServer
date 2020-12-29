7b03a9e31c1c0560d3d1102ef78922d96de45219
yangkailun <2362080354@qq.com>
2020-12-29 10:49:56 +0800

    修改导入重复问题

--- blade-service/mk-adata/src/main/java/org/springblade/adata/service/impl/ExpertServiceImpl.java
+++ blade-service/mk-adata/src/main/java/org/springblade/adata/service/impl/ExpertServiceImpl.java
@@ -347,6 +347,9 @@ public class ExpertServiceImpl extends BaseServiceImpl<ExpertMapper, Expert> imp
 		parameters.put("searchType", "all");
 		parameters.put("offset", offset);
 		parameters.put("size", size);
+		JSONArray sorts = new JSONArray();
+		sorts.add("_id");
+		parameters.put("sorts",sorts);
 
 
 		JSONObject schema = new JSONObject();
