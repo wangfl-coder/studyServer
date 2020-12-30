029a8bd238c5865898da0c67281a71f1aadc1140
wang9702 <862741851@qq.com>
2020-12-30 11:21:48 +0800

    质检任务增加时间和图片字段以及更新的feign接口

--- blade-service-api/mk-task-api/src/main/java/org/springblade/task/feign/IQualityInspectionTaskClient.java
+++ blade-service-api/mk-task-api/src/main/java/org/springblade/task/feign/IQualityInspectionTaskClient.java
@@ -6,14 +6,26 @@ import org.springblade.task.entity.LabelTask;
 import org.springblade.task.entity.QualityInspectionTask;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = LauncherConstant.MKAPP_TASK_NAME)
 public interface IQualityInspectionTaskClient {
 
 	String API_PREFIX = "/client";
-	String QUERY_QUALITY_INSPECTION_TASK = API_PREFIX + "/query-quality-inspection-task";
+	String QUERY_QUALITY_INSPECTION_TASK_BY_INSTANCEID = API_PREFIX + "/query-quality-inspection-task-by-instance-id";
+	String QUERY_QUALITY_INSPECTION_TASK_BY_ID = API_PREFIX + "/query-quality-inspection-task-by-id";
+	String UPDATE_QUALITY_INSPECTION_TASK_BY_ID = API_PREFIX + "/update-quality-inspection-task-by-id";
 
-	@GetMapping(QUERY_QUALITY_INSPECTION_TASK)
+	@GetMapping(QUERY_QUALITY_INSPECTION_TASK_BY_INSTANCEID)
 	R<QualityInspectionTask> queryQualityInspectionTask(@RequestParam(value = "processInstanceId") String processInstanceId);
+
+	@GetMapping(QUERY_QUALITY_INSPECTION_TASK_BY_ID)
+	R<QualityInspectionTask> queryQualityInspectionTaskById(@RequestParam(value = "inspectionId") Long inspectionId);
+
+	@PostMapping(UPDATE_QUALITY_INSPECTION_TASK_BY_ID)
+	R updateQualityInspectionTaskById(@RequestBody QualityInspectionTask qualityInspectionTask);
+
+
 }
