1d3717386dd50828502dfddb5577a689eaff7d05
wangshan <shan.wang@aminer.cn>
2021-03-26 10:28:08 +0800

    增加userlog,未自测

---
+++ blade-service-api/mk-log-api/src/main/java/org/springblade/log/cache/UserLogCache.java
@@ -0,0 +1,87 @@
+/*
+ *      Copyright (c) 2018-2028, Chill Zhuang All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  Neither the name of the dreamlu.net developer nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ *  Author: Chill 庄骞 (smallchill@163.com)
+ */
+package org.springblade.log.cache;
+
+import org.springblade.core.cache.utils.CacheUtil;
+import org.springblade.core.tool.api.R;
+import org.springblade.core.tool.utils.Func;
+import org.springblade.core.tool.utils.SpringUtil;
+import org.springblade.core.tool.utils.StringPool;
+import org.springblade.core.tool.utils.StringUtil;
+import org.springblade.log.entity.UserLog;
+import org.springblade.log.feign.IUserLogClient;
+
+import static org.springblade.core.launch.constant.FlowConstant.TASK_USR_PREFIX;
+
+/**
+ * 系统缓存
+ *
+ * @author Chill
+ */
+public class UserLogCache {
+	private static final String USERLOG_CACHE = "mk:userlog";
+	private static final String USERLOG_CACHE_ID = "userlog:id:";
+	private static final String USERLOG_CACHE_ACCOUNT = "userlog:account:";
+
+	private static IUserLogClient userClient;
+
+	private static IUserLogClient getUserClient() {
+		if (userClient == null) {
+			userClient = SpringUtil.getBean(IUserLogClient.class);
+		}
+		return userClient;
+	}
+
+	/**
+	 * 根据任务用户id获取用户信息
+	 *
+	 * @param taskUserId 任务用户id
+	 * @return
+	 */
+//	public static UserLog getUserByTaskUser(String taskUserId) {
+//		Long userId = Func.toLong(StringUtil.removePrefix(taskUserId, TASK_USR_PREFIX));
+//		return getUser(userId);
+//	}
+
+	/**
+	 * 获取用户
+	 *
+	 * @param userId 用户id
+	 * @return
+	 */
+	public static UserLog getUserLog(Long userId) {
+		return CacheUtil.get(USERLOG_CACHE, USERLOG_CACHE_ID, userId, () -> {
+			R<UserLog> result = getUserClient().userInfoById(userId);
+			return result.getData();
+		});
+	}
+
+	/**
+	 * 获取用户
+	 *
+	 * @param tenantId 租户id
+	 * @param account  账号名
+	 * @return
+	 */
+	public static UserLog getUserLog(String tenantId, String account) {
+		return CacheUtil.get(USERLOG_CACHE, USERLOG_CACHE_ACCOUNT, tenantId + StringPool.DASH + account, () -> {
+			R<UserLog> result = getUserClient().userByAccount(tenantId, account);
+			return result.getData();
+		});
+	}
+
+}
